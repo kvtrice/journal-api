8036c455af2bdf6cb17d2f1fc9ff02316171aa8f
kvtrice <katricemountford@gmail.com>
2024-02-12 10:54:57 +1100

    Renamed index to app.js and moved port listen to index file

---
+++ app.js
@@ -0,0 +1,20 @@
+import express from 'express'
+import entryRoutes from './routes/entry_routes.js'
+import categoryRoutes from './routes/category_routes.js'
+import cors from 'cors'
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+// Index route
+app.get('/', (req, res) => res.send({info: "Journal API"}))
+
+// Category Routes Router
+app.use(categoryRoutes)
+
+// Entry Routes Router
+app.use('/entries', entryRoutes)
+
+export default app
--- index.js
+++ index.js
@@ -1,21 +1,4 @@
-import express from 'express'
-import entryRoutes from './routes/entry_routes.js'
-import categoryRoutes from './routes/category_routes.js'
-import cors from 'cors'
-
-const app = express()
-
-app.use(cors())
-app.use(express.json())
-
-// Index route
-app.get('/', (req, res) => res.send({info: "Journal API"}))
-
-// Category Routes Router
-app.use(categoryRoutes)
-
-// Entry Routes Router
-app.use('/entries', entryRoutes)
+import app from './app.js'
 
 // Listen
 app.listen(4000)
